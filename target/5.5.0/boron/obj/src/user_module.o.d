c126d858364ec5ddf8f58773dd35ac2c91f5c470
skorm11x <kosik.chris.j@gmail.com>
2023-10-16 14:34:18 -0600

    updated pictures

---
+++ target/5.5.0/boron/obj/src/user_module.o.d
@@ -0,0 +1,7 @@
+/Volumes/s_ssd_ext/codes/particle_deviceos/KNELL/target/5.5.0/boron//obj/./src/user_module.o: \
+ src/user_module.c \
+ ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
+ ../../../dynalib/inc/dynalib.h ../../../hal/shared/platforms.h
+../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
+../../../dynalib/inc/dynalib.h:
+../../../hal/shared/platforms.h:
