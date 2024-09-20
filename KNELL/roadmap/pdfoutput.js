5a4951f5e47a0e1189fc3b195244dab94c55715c
skorm11x <kosik.chris.j@gmail.com>
2024-09-20 10:52:42 -0600

    created basic roadmap for gain submission.

---
+++ KNELL/roadmap/pdfoutput.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+const path = require('path');
+
+(async () => {
+  const filePath = path.resolve(__dirname, 'index.html');
+  const fileUrl = `file://${filePath}`;
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(fileUrl, { waitUntil: 'networkidle0' });
+  await page.pdf({ path: 'roadmap.pdf', format: 'A4' });
+  await browser.close();
+})();
