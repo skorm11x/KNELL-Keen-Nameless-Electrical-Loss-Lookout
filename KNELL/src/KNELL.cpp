4145b9b3b59d55e700a67fa0a28347a95c73c170
Edgard Torres <edgard.torres.ctr@us.af.mil>
2025-05-20 09:42:23 -0600

    clang format and added missing sucribe to to get_weather

--- KNELL/src/KNELL.cpp
+++ KNELL/src/KNELL.cpp
@@ -9,15 +9,15 @@
 // #define POWER_DEBUG
 // #define CELLULAR_DEBUG
 
-#include <stdint.h>
 #include "DiagnosticsHelperRK.h"
+#include <stdint.h>
 // #include "assert.h"
 #include "SystemDefinitions.h"
 
 SYSTEM_MODE(AUTOMATIC);
 SYSTEM_THREAD(ENABLED);
 
-particle::Future<bool> publish(const char* name, const char* data);
+particle::Future<bool> publish(const char *name, const char *data);
 FuelGauge fuel;
 SerialLogHandler logHandler;
 uint64_t lastSync;
@@ -40,49 +40,48 @@ void get_weather();
 void setup();
 void loop();
 
-
 void setup() {
-  Particle.variable("battery_voltage", battery_voltage);
+    Particle.variable("battery_voltage", battery_voltage);
+    Particle.subscribe("hook-response/get_weather", get_weather_handler, MY_DEVICES);
 
-  if (Cellular.ready()) {
-      CellularSignal sig = Cellular.RSSI();
-      cell_sig_str = sig.getStrength();
-      cell_sig_qual = sig.getQuality();
+    if (Cellular.ready()) {
+        CellularSignal sig = Cellular.RSSI();
+        cell_sig_str = sig.getStrength();
+        cell_sig_qual = sig.getQuality();
       #ifdef CELLULAR_DEBUG
         Log.info("Cellular ready at startup: %f strength and %f quality",
-          cell_sig_str, cell_sig_qual);
+                 cell_sig_str, cell_sig_qual);
         Log.info("localIP: %s", Cellular.localIP().toString().c_str());
         Serial.begin(9600);
       #endif
-      lastSync = millis();
-      Serial.begin(9600);
+        lastSync = millis();
+        Serial.begin(9600);
     }
 }
 
-
 void loop() {
-  get_battery_voltage();
-  check_day_time_sync();
-  detect_power_state();
-  get_weather();
-  delay(loopDelayTime);
+    get_battery_voltage();
+    check_day_time_sync();
+    detect_power_state();
+    get_weather();
+    delay(loopDelayTime);
 }
 
 /*
   Reads battery voltage and updates stack value
 */
 void get_battery_voltage() {
-  battery_voltage = fuel.getVCell();
+    battery_voltage = fuel.getVCell();
 }
 
 /*
   Checked at top of every loop, sync time with cloud at least daily.
 */
 void check_day_time_sync() {
-  if (millis() - lastSync > ONE_DAY_MILLIS) {
-    Particle.syncTime();
-    lastSync = millis();
-  }
+    if (millis() - lastSync > ONE_DAY_MILLIS) {
+        Particle.syncTime();
+        lastSync = millis();
+    }
 }
 
 /*
@@ -90,45 +89,44 @@ void check_day_time_sync() {
   See POWER_CODES structure for string messages associated with message reads.
 */
 void detect_power_state() {
-  powerSource = DiagnosticsHelper::getValue(DIAG_ID_SYSTEM_POWER_SOURCE);
-  #ifdef POWER_DEBUG
+    powerSource = DiagnosticsHelper::getValue(DIAG_ID_SYSTEM_POWER_SOURCE);
+    #ifdef POWER_DEBUG
     Log.info("power src str: %s", powerSourceStr.c_str());
     Log.info("power src INT: %d", powerSource);
-  #endif
+    #endif
     if (powerSource != initPowerSource && powerSource != lastPowerSource) {
-        #ifdef POWER_DEBUG
-          Log.info("Potential Power source change: %s", powerSourceStr.c_str());
-        #endif
+    #ifdef POWER_DEBUG
+        Log.info("Potential Power source change: %s", powerSourceStr.c_str());
+    #endif
         delay(loopDelayTime);
         powerSource = DiagnosticsHelper::getValue(DIAG_ID_SYSTEM_POWER_SOURCE);
         if (powerSource != initPowerSource) {
-          #ifdef POWER_DEBUG
+        #ifdef POWER_DEBUG
             Log.info("Confirmed Power source change: %s",
-              powerSourceStr.c_str());
-          #endif
-          if (powerSource == 5) {
-            publish_power_loss();
-          }
-          if (powerSource == 1) {
-            publish_power_restored();
-          }
-          if (powerSource == 2 || powerSource == 3 || powerSource == 4) {
-            publish_power_restored();
-          }
+                     powerSourceStr.c_str());
+        #endif
+            if (powerSource == 5) {
+                publish_power_loss();
+            }
+            if (powerSource == 1) {
+                publish_power_restored();
+            }
+            if (powerSource == 2 || powerSource == 3 || powerSource == 4) {
+                publish_power_restored();
+            }
         }
     }
-    if (powerSource == initPowerSource && lastPowerSource
-        != powerSource && lastPowerSource != -1) {
+    if (powerSource == initPowerSource && lastPowerSource != powerSource && lastPowerSource != -1) {
       #ifdef POWER_DEBUG
         Log.info("Potential Power source change: %s", powerSourceStr.c_str());
       #endif
-      delay(loopDelayTime);
-      powerSource = DiagnosticsHelper::getValue(DIAG_ID_SYSTEM_POWER_SOURCE);
+        delay(loopDelayTime);
+        powerSource = DiagnosticsHelper::getValue(DIAG_ID_SYSTEM_POWER_SOURCE);
 
-      if (powerSource == initPowerSource) {
-        publish_power_restored();
-      }
-  }
+        if (powerSource == initPowerSource) {
+            publish_power_restored();
+        }
+    }
 }
 
 /*
@@ -136,12 +134,12 @@ void detect_power_state() {
   cloud. We keep trying until successful ACK
 */
 void publish_power_loss() {
-  status = String::format("{\"powerSource\":\"%s\"}", powerLossStr);
-  success = Particle.publish("power_change", status, PRIVATE, WITH_ACK);
-  lastPowerSource = powerSource;
-  while (!success) {
+    status = String::format("{\"powerSource\":\"%s\"}", powerLossStr);
     success = Particle.publish("power_change", status, PRIVATE, WITH_ACK);
-  }
+    lastPowerSource = powerSource;
+    while (!success) {
+        success = Particle.publish("power_change", status, PRIVATE, WITH_ACK);
+    }
 }
 
 /*
@@ -149,12 +147,12 @@ void publish_power_loss() {
   cloud. We keep trying until successful ACK
 */
 void publish_power_restored() {
-  status = String::format("{\"powerSource\":\"%s\"}", powerRestoredStr);
-  success = Particle.publish("power_change", status, PRIVATE, WITH_ACK);
-  while (!success) {
+    status = String::format("{\"powerSource\":\"%s\"}", powerRestoredStr);
     success = Particle.publish("power_change", status, PRIVATE, WITH_ACK);
-  }
-  lastPowerSource = powerSource;
+    while (!success) {
+        success = Particle.publish("power_change", status, PRIVATE, WITH_ACK);
+    }
+    lastPowerSource = powerSource;
 }
 
 /*
@@ -162,8 +160,8 @@ void publish_power_restored() {
   Note: Epoch time 6 hours a head - 8:00 local --> 14:00 Epoch
 */
 void get_weather() {
-    //Log.info("Time is: %i:%i:%i:", Time.hour(), Time.minute(), Time.second());
-    if (Time.hour() == 14 && Time.minute() == 0 && Time.second() < 5) {
+    // Log.info("Time is: %i:%i:%i:", Time.hour(), Time.minute(), Time.second());
+    if (Time.hour() == 15 && Time.minute() == 0 && Time.second() < 5) {
         success = Particle.publish("get_weather", PRIVATE, WITH_ACK);
 
         while (!success) {
@@ -289,4 +287,3 @@ void post_weather(String period) {
     // wait 1 sec. before sending next call to pushover cloud, experiencing periods out of order.
     delay(1000);
 }
-
